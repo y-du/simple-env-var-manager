71e5d58df58ab27a7f6b90c297432cee663156da
y-du <42994541+y-du@users.noreply.github.com>
2022-03-03 20:17:51 +0100

    add tests

---
+++ tests/test_config.py
@@ -0,0 +1,105 @@
+"""
+   Copyright 2022 Yann Dumont
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+"""
+
+import unittest
+import os
+import sevm
+
+
+test_str = "test"
+
+
+class Config(sevm.Config):
+    var_a = None
+    var_b = test_str
+
+    class Section(sevm.Config):
+        var_c = None
+        var_d = test_str
+
+
+class TestConfig(unittest.TestCase):
+    def test_default_value(self):
+        config = Config()
+        self.assertEqual(config.var_b, test_str)
+
+    def test_override_value_str(self):
+        os.environ["VAR_A"] = test_str
+        config = Config()
+        self.assertEqual(config.var_a, test_str)
+
+    def test_override_value_empty_str(self):
+        os.environ["VAR_A"] = ""
+        config = Config()
+        self.assertEqual(config.var_a, "")
+
+    def test_override_value_int(self):
+        os.environ["VAR_A"] = "0"
+        config = Config()
+        self.assertEqual(config.var_a, 0)
+
+    def test_override_value_negative_int(self):
+        os.environ["VAR_A"] = "-1"
+        config = Config()
+        self.assertEqual(config.var_a, -1)
+
+    def test_override_value_float(self):
+        os.environ["VAR_A"] = "0.0"
+        config = Config()
+        self.assertEqual(config.var_a, 0.0)
+
+    def test_override_value_negative_float(self):
+        os.environ["VAR_A"] = "-1.0"
+        config = Config()
+        self.assertEqual(config.var_a, -1.0)
+
+    def test_override_value_complex(self):
+        os.environ["VAR_A"] = "3+5j"
+        config = Config()
+        self.assertEqual(config.var_a, (3+5j))
+
+    def test_override_value_true(self):
+        os.environ["VAR_A"] = "true"
+        config = Config()
+        self.assertEqual(config.var_a, True)
+
+    def test_override_value_false(self):
+        os.environ["VAR_A"] = "false"
+        config = Config()
+        self.assertEqual(config.var_a, False)
+
+    def test_section_default_value(self):
+        config = Config()
+        self.assertEqual(config.Section.var_d, test_str)
+
+    def test_section_override_value_str(self):
+        os.environ["VAR_C"] = test_str
+        config = Config()
+        self.assertEqual(config.Section.var_c, test_str)
+
+    def test_lower_case_keys(self):
+        os.environ["var_a"] = test_str
+        config = Config(upper_keys=False)
+        self.assertEqual(config.var_a, test_str)
+
+    def test__section_lower_case_keys(self):
+        os.environ["var_c"] = test_str
+        config = Config(upper_keys=False)
+        self.assertEqual(config.Section.var_c, test_str)
+
+
+if __name__ == '__main__':
+    unittest.main()
