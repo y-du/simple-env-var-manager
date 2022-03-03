5bac878afa3c977a3317056bdb767701934a2663
y-du <42994541+y-du@users.noreply.github.com>
2022-03-03 20:24:38 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -19,31 +19,31 @@ import setuptools
 
 def read_metadata(file):
     metadata = dict()
-    with open(file, 'r') as init_file:
+    with open(file, "r") as init_file:
         for line in init_file.readlines():
-            if line.startswith('__'):
-                line = line.replace("'", '')
-                line = line.replace('\n', '')
-                key, value = line.split(' = ')
+            if line.startswith("__"):
+                line = line.replace("\"", "")
+                line = line.replace("\n", "")
+                key, value = line.split(" = ")
                 metadata[key] = value
     return metadata
 
 
-metadata = read_metadata('sevm/__init__.py')
+metadata = read_metadata("sevm/__init__.py")
 
 setuptools.setup(
-    name=metadata.get('__title__'),
-    version=metadata.get('__version__'),
-    author=metadata.get('__author__'),
-    description=metadata.get('__description__'),
-    license=metadata.get('__license__'),
-    url=metadata.get('__url__'),
-    copyright=metadata.get('__copyright__'),
+    name=metadata.get("__title__"),
+    version=metadata.get("__version__"),
+    author=metadata.get("__author__"),
+    description=metadata.get("__description__"),
+    license=metadata.get("__license__"),
+    url=metadata.get("__url__"),
+    copyright=metadata.get("__copyright__"),
     packages=setuptools.find_packages(exclude=("tests", )),
-    python_requires='>=3.5.3',
+    python_requires=">=3.5.3",
     classifiers=[
-        'Intended Audience :: Developers',
-        'Operating System :: Unix',
-        'Natural Language :: English',
+        "Intended Audience :: Developers",
+        "Operating System :: Unix",
+        "Natural Language :: English",
     ]
 )
